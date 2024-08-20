1ca98303bcde0eda17a0e95d313841fee1f5d42f
Prio <prio96@icloud.com>
2024-08-21 01:06:11 +0600

    Set up the project for trying Django Template Filters

--- PracticeProject/FirstApp/views.py
+++ PracticeProject/FirstApp/views.py
@@ -2,5 +2,32 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    d={}
+    d={"Name":'California Institute of Technology',"Abbreviation":"CALTECH","Location":"Pasadena, California, USA","Students":[
+        {
+            "Name":"Sheldon Cooper",
+            "Id":1,
+            "Department":"Physics"
+        },
+        {
+            "Name":"Leonard Hofstadter",
+            "Id":2,
+            "Department":"Physics"
+        },
+        {
+            "Name":"Raj Koothrappali",
+            "Id":3,
+            "Department":"Astrophysics"
+        },
+        {
+            "Name":"Howard Wolowitz",
+            "Id":4,
+            "Department":"Mechanical Engineering"
+        },
+        {
+            "Name":"Bernadette Rostenkowski",
+            "Id":5,
+            "Department":"Microbiology"
+        },
+        
+    ]}
     return render(request,"FirstApp/home.html",d)
