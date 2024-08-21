622a2fd318848e11543a5fbafec08f4b695e3311
Prio <prio96@icloud.com>
2024-08-21 23:06:42 +0600

    Practiced many Django Template Filters

--- PracticeProject/FirstApp/views.py
+++ PracticeProject/FirstApp/views.py
@@ -1,32 +1,38 @@
 from django.shortcuts import render
+from datetime import *
 
 # Create your views here.
 def home(request):
-    d={"Name":'California Institute of Technology',"Abbreviation":"CALTECH","Location":"Pasadena, California, USA","Students":[
+    d={"Name":'California Institute of Technology',"Abbreviation":"CALTECH","Location":"Pasadena, California, USA","Publish_Date":datetime.now()-timedelta(days=5),"Edit_Date":datetime.now()-timedelta(days=2),"Students":[
         {
             "Name":"Sheldon Cooper",
-            "Id":1,
-            "Department":"Physics"
+            "Id":0,
+            "Department":"Physics",
+            "Club":"Chess Club"
         },
         {
             "Name":"Leonard Hofstadter",
-            "Id":2,
-            "Department":"Physics"
+            "Id":1,
+            "Department":"Physics",
+            "Club":""
         },
         {
             "Name":"Raj Koothrappali",
-            "Id":3,
-            "Department":"Astrophysics"
+            "Id":2,
+            "Department":"Astrophysics",
+            "Club":"Space Club"
         },
         {
             "Name":"Howard Wolowitz",
-            "Id":4,
-            "Department":"Mechanical Engineering"
+            "Id":3,
+            "Department":"Mechanical Engineering",
+            "Club":"IT Club"
         },
         {
             "Name":"Bernadette Rostenkowski",
-            "Id":5,
-            "Department":"Microbiology"
+            "Id":4,
+            "Department":"Microbiology",
+            "Club":"Wildlife Club"
         },
         
     ]}
